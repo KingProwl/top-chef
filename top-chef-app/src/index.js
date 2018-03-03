07e5e5389e35ef2afc2ecb634a580e4600199b4b
KingProwl <paul.ghafari@wanadoo.fr>
2018-03-03 20:14:35 +0100

    Import Bootstrap in index.js

--- top-chef-app/src/index.js
+++ top-chef-app/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'; //import bootstrap.
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
